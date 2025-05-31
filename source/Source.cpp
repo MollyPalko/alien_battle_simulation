144b3cee1e00050bd72b17a1dc16b303ac9c0810
mpalko26 <mpalko26@unm.edu>
2025-05-30 22:23:42 -0600

    pt 3 complete - main method refactored for inheritance

--- source/Source.cpp
+++ source/Source.cpp
@@ -1,27 +1,18 @@
 #include <ctime>
 #include <iostream>
 #include "Boov.h"
+#include "Competitor.h"
 #include "Gorg.h"
 
 int main()
 {
   // different randomness every time program is executed
   srand(static_cast<unsigned> (time(0)));
-/*
-  // get health
-  std::cout << "Enter Boov health value: ";
-  int health;
-  std::cin >> health;
-
-  // get shield;
-  std::cout << "Enter Boov shield value: ";
-  int shield;
-  std::cin >> shield;
-*/
+  
   // Run many simulations
   const int NUMBER_OF_SIMULATIONS = 100000;
+  
   // count total num rounds survived
-  //int roundsSurvived = 0;
   int boovWins = 0;
   int gorgWins = 0;
 
@@ -29,20 +20,21 @@ int main()
   for (int i = 0; i <= NUMBER_OF_SIMULATIONS; ++i)
   {
     // how long can a single Boov last
-    Boov oh("Oh", 31, 13);
-    Gorg george; // calls default constructor
+    // use pointers here to say figure out at runtime which version of virtual function to use
+    Competitor * oh = new Boov("Oh", 31, 13);
+    Competitor * george = new Gorg; // calls default constructor
 
     // while still healthy enough to do battle...
-    while ((oh.IsDefeated() == false) && (george.IsDefeated() == false))
+    while ((oh->IsDefeated() == false) && (george->IsDefeated() == false))
     {
-      george.GetsAttacked();
-      if (george.IsDefeated() == false)
+      george->GetsAttacked();
+      if (george->IsDefeated() == false)
       {
-        oh.GetsAttacked();
+        oh->GetsAttacked();
       }
     }
 
-    if (george.IsDefeated())
+    if (george->IsDefeated())
     {
       ++boovWins;
     }
@@ -57,14 +49,6 @@ int main()
 
   std::cout << "Boov: " << boov_pct << std::endl;
   std::cout << "Gorg: " << gorg_pct << std::endl;
-
-  // gather and report final statistics
-  //std::cout << "Average number of rounds until defeated: ";
-
-
-  //double average = roundsSurvived / static_cast<double>(NUMBER_OF_SIMULATIONS);
-
-  //std::cout << average << std::endl;
 
   return 0;
 }
